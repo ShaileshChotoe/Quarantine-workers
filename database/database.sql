fdde556c9d774e39468309ba20be99242be49085
unknown <2120367@talnet.nl>
2020-03-27 14:36:42 +0100

    branch switch test

---
+++ database/database.sql
@@ -0,0 +1,38 @@
+create DATABASE
+if not EXISTS quarantine;
+
+use quarantine;
+
+create TABLE
+if EXISTS users
+(
+    id int AUTO_INCREMENT,
+    username varchar
+(100),
+    password varchar
+(100),
+    posts int
+(100),
+    PRIMARY KEY
+(id)
+);
+
+create table
+if EXISTS posts
+(
+    id int AUTO_INCREMENT,
+    code text
+(1),
+    language varchar
+(25),
+    private boolean,
+    link varchar
+(255),
+    PRIMARY KEY
+(id)
+);
+
+insert into users
+    (username, password, posts)
+VALUES
+    ('admin', '123', 0);
