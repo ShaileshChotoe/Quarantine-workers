8adab71575623f8f28a26d11916427f1bc82b210
GeorgeWahba <george-wahba@hotmail.com>
2020-03-31 14:33:02 +0200

    “public”

--- public/pages/frontpage/index.php
+++ public/pages/frontpage/index.php
@@ -4,12 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Totalcode</title>
-    <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="index">
+        
+        <button onclick="logout()" class="logout">Log uit</button>
         <div class="public">
             <h1 class="tekst">Hier alle openbare dingen</h1>
+            <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
+            <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
+            <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
+            <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
+            <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
+            <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
+            <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
             <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
             <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
             <a href="hier het id" class="publiccode">en dan hier de code, vergeet de id niet</a>
